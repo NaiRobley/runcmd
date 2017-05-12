6ccb991fde85ab4045c9b728ed2cd303d0fd2093
jmm otuya <jmm otuya>
2017-05-12 13:35:34 +0300

    created 'if exists' check condition

--- calendar.py
+++ calendar.py
@@ -15,7 +15,21 @@ class Calendar(object):
     def see_events(self):
         """Naomi"""
         pass
+#################################################################################################################################
+############# CALENDAR TEAM PROJECT: IDD-HANDLING VIEW LAST EVENT IN THE CALENDAR ###############################################
+#################################################################################################################################
+    def view_last_event(self, cal_name):
+        """get calendar and find events in the calendar, return last event"""
+        # calendar_name=self.calendar_name
+        event_list=0
+        last_event=""
+        if cal_name not in self.calendars:
+        	print("This calendar does not exisit")
+        else:
+        	event_list=self.calendars.get(cal_name)
+        	last_event=event_list[-1]
+        	return last_event
 
-    def view_last_event(self):
-        """Idd"""
-        pass
+newCal = Calendar()
+newCal.view_last_event('first_calendar')
+newCal.view_last_event('another')
