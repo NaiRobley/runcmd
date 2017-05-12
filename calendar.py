5ae0fccb45288bbb4ffd14301419af27f987f415
kamwerunaomi <kamwerunaomi@gmail.com>
2017-05-12 13:51:03 +0300

    Revert "The code gets the calendar name and prints out the list of events it …"

--- calendar.py
+++ calendar.py
@@ -1,8 +1,7 @@
 class Calendar(object):
     def __init__(self):
         self.calendars = {
-                            'first_calendar': ['first_event', 'second_event'],
-                            'second_calendar':['first_event','second_event']
+                            'first_calendar': ['first_event']
                             }
 
     def create_calendar(self):
@@ -13,21 +12,10 @@ class Calendar(object):
         """Ledduh"""
         pass
 
-    def see_events(self, cal_name):
+    def see_events(self):
         """Naomi"""
-        #gets the calendar name
-        #gets the events as per that calendar
-        #prints out the events for the calendar
-        try:
-            events=self.calendars[cal_name]
-            print("The events are:")
-            for event in events:
-                print("{}".format(event))
-        except KeyError:
-            print("Sorry, the calendar name does not exist")
+        pass
+
     def view_last_event(self):
         """Idd"""
         pass
-
-new_cal=Calendar()
-new_cal.see_events("third calendar")
