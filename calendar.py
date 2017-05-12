a5811ab4a422d2095aa01c197ff64c08c51b9de5
Robley Gori <robleyadrian@gmail.com>
2017-05-12 14:57:14 +0300

    Changed the format of messages

--- calendar.py
+++ calendar.py
@@ -1,33 +1,39 @@
 class Calendar(object):
     def __init__(self):
-        self.calendars = {
-                            'first_calendar': ['first_event', 'second_event'],
-                            'second_calendar':['first_event','second_event']
-                            }
+        self.calendars = {}
 
     def create_calendar(self,calendar_name):
         """Ledduh"""
         self.calendars[calendar_name] = []
-        print("New calendar has been created") 
-        return self.calendars 
+        print('')
+        print("\tNew calendar {} has been created".format(calendar_name))
+        print('')
+        return self.calendars
 
     def add_event(self, calendar_name, event_name):
         """Ledduh"""
         self.calendars[calendar_name].append(event_name)
-        print("New event has been added")
+        print('')
+        print("\tNew event {0} has been added to the calendar {1}".format(event_name, calendar_name))
+        print('')
 
     def see_events(self, cal_name):
         """Naomi"""
         #gets the calendar name
         #gets the events as per that calendar
         #prints out the events for the calendar
+        print('')
         try:
             events=self.calendars[cal_name]
-            print("The events are:")
-            for event in events:
-                print("{}".format(event))
+            if len(events) > 0:
+                print("\tThe events in {}:".format(cal_name))
+                for event in events:
+                    print("\t\t{}".format(event))
+            else:
+                print("\tThere are currently no events in calendar {}".format(cal_name))
         except KeyError:
-            print("Sorry, the calendar name does not exist")
+            print("\tSorry, the calendar name does not exist")
+        print('')
 
 #################################################################################################################################
 ############# CALENDAR TEAM PROJECT: IDD-HANDLING VIEW LAST EVENT IN THE CALENDAR ###############################################
@@ -38,10 +44,9 @@ class Calendar(object):
         event_list=0
         last_event=""
         if cal_name not in self.calendars:
-        	print("This calendar does not exisit")
+        	print("\n\tThis calendar does not exist\n")
         else:
         	event_list=self.calendars.get(cal_name)
         	last_event=event_list[-1]
-        	print (last_event)
+        	print ("\n\tThe last event in {0} is: {1}\n".format(cal_name, last_event))
         	return last_event
-
