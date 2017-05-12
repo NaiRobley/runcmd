28d6a352b41b224d3a82c6acd618947e8396dbac
Robley Gori <robleyadrian@gmail.com>
2017-05-12 12:25:53 +0300

    Created the calendar class boilerplate

---
+++ calendar.py
@@ -0,0 +1,21 @@
+class Calendar(object):
+    def __init__(self):
+        self.calendars = {
+                            'first_calendar': ['first_event']
+                            }
+
+    def create_calendar(self):
+        """Ledduh"""
+        pass
+
+    def add_event(self):
+        """Ledduh"""
+        pass
+
+    def see_events(self):
+        """Naomi"""
+        pass
+
+    def view_last_event(self):
+        """Idd"""
+        pass
